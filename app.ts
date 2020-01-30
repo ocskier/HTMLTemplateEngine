6f5f317675a3e4367217b6c7b391665dad37625f
ocskier <ocskier@gmail.com>
2020-01-30 01:01:03 -0500

    loop & switch for inquirer input, loop employee ejs cards

--- app.ts
+++ app.ts
@@ -16,43 +16,75 @@ const Manager = require('./lib/Manager');
 const writeFile = util.promisify(fs.writeFile);
 
 // Main functional logic
-
 const init = async () => {
+  let i = 0;
+  const newEmployees = [];
+  while (i < 5) {
     const team = await ask.prompt([
-        {
-            message: "Enter your name: ",
-            name: "name"
+      {
+        message: 'Enter your name: ',
+        name: 'name',
+      },
+      {
+        message: 'Enter your email: ',
+        name: 'email',
+      },
+      {
+        message: 'Whats your role: ',
+        name: 'role',
+        validate: async (input: string) => {
+          if (
+            input !== 'Engineer' &&
+            input !== 'Intern' &&
+            input !== 'Manager'
+          ) {
+            return 'Incorrect asnwer';
+          }
+          return true;
         },
-        {
-            message: "Enter your email: ",
-            name: "email"
-        },
-        {
-            message: "Github Username: ",
-            name: "githubID"
-        }
+      },
+      {
+        message: 'Github Username: ',
+        name: 'githubID',
+      },
     ]);
-    const newEmployees = [];
-    newEmployees.push(new Engineer(team.name, team.email, team.githubID));
+    switch (team.role) {
+      case 'Engineer':
+        newEmployees.push(new Engineer(team.name, team.email, team.githubID));
+        break;
+      case 'Intern':
+        newEmployees.push(new Intern(team.name, team.email, 'LRHS'));
+        break;
+      case 'Manager':
+        newEmployees.push(
+          new Manager(
+            team.name,
+            team.email,
+            Math.floor(Math.random() * 100) + 1
+          )
+        );
+        break;
+    }
     console.log(newEmployees);
-    render(newEmployees);
-}
+    i++;
+  }
+  render(newEmployees);
+};
 
 init();
 
-async function render(emp: any) {
+async function render(empArr: any[]) {
   try {
-    let html = '';
-    for (let i = 0; i < emp.length; i++) {
-        let employeeHtml = await ejs.renderFile('./templates/engineer.ejs', { data: emp[i] });
-        html += employeeHtml;
-    }
-    const mainHtml = await ejs.renderFile('./templates/main.ejs', {data: {main: html}});
-    console.log(mainHtml);
+    let html = await ejs.renderFile('./templates/engineer.ejs', {
+      data: empArr,
+    });
+    const mainHtml = await ejs.renderFile('./templates/main.ejs', {
+      data: { main: html },
+    });
     await writeFile('dist/index.html', mainHtml, 'UTF-8');
   } catch (error) {
     console.log(error);
   }
 }
 
-export{}
+export {};
