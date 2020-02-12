333921d547d836985921171e967750d26618fd2e
ocskier <ocskier@gmail.com>
2020-02-11 23:23:49 -0500

    removing redundant inquirer

--- app.ts
+++ app.ts
@@ -1,8 +1,3 @@
-const inquirer = require('inquirer');
-const validator = require('validator');
-
-// const ui = new inquirer.ui.BottomBar();
-
 // Internal node package imports
 const fs = require('fs');
 const util = require('util');
@@ -10,6 +5,8 @@ const util = require('util');
 // External package imports
 const ask = require('inquirer');
 const ejs = require('ejs');
+const validator = require('validator');
+// const ui = new inquirer.ui.BottomBar();
 
 // Import the individual classes
 const Engineer = require('./lib/Engineer');
@@ -124,14 +121,14 @@ const getSubordinates = async () => {
 
     switch (role) {
       case 'Engineer':
-        const engineer = await inquirer.prompt(engineerQues);
+        const engineer = await ask.prompt(engineerQues);
         engineer &&
           newEmployees.push(
             new Engineer(engineer.name, engineer.email, engineer.githubID)
           );
         break;
       case 'Intern':
-        const intern = await inquirer.prompt(internQues);
+        const intern = await ask.prompt(internQues);
         intern &&
           newEmployees.push(
             new Intern(intern.name, intern.email, intern.school)
